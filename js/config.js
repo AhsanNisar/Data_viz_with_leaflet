b6d7e9d8a03329ea0ba05a236f04cba84012b69b
AhsanNisar <ahsan_muhammad1@hotmail.com>
2019-05-28 22:03:27 -0500

    added index.html, config.js and style.css file

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibXVoYW1tYWRhaHNhbjEiLCJhIjoiY2p2am9oMDVrMGM5ZDN5cWwxOTBibGg2YyJ9.L-v_9hHwOlIzP8_D1n6L1g";
+
+// API key
+//const API_KEY = "pk.eyJ1IjoibWFubGFyYSIsImEiOiJjamtnd2R3ZW4wbWlwM3FxZ3BoY2JkNm1qIn0.NQ7yhFgYEP0fqdHkT9c6-Q";
